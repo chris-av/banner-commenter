eb22af1abc5f8bf29e9d262820557a5a015587b5
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2024-04-12 19:04:47 -0700

    reformat

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
+#include "version.h"
 #include <ctype.h>
+#include <getopt.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
-#include <getopt.h>
-#include "version.h"
-
 
 void cpyargs(int n, char *args_orig[], char *args_cpy[]) {
   for (int i = 0; i < n; i++) {
@@ -46,35 +45,41 @@ int main(int argc, char *argv[]) {
   int opt;
 
   struct option long_options[] = {
-    { "help", no_argument, NULL, 'h' },
-    { "message", no_argument, NULL, 'm' },
-    { "version", no_argument, NULL, 'v' },
-    { NULL, 0, NULL, 0 },
+      {"help", no_argument, NULL, 'h'},
+      {"message", no_argument, NULL, 'm'},
+      {"version", no_argument, NULL, 'v'},
+      {NULL, 0, NULL, 0},
   };
 
   char *message = NULL;
 
   while ((opt = getopt_long(argc, argv, "hvm:", long_options, NULL)) != -1) {
     switch (opt) {
-      case 'h':
-        printf("\n");
-        printf("  -h, --help       help, run `man banner-commenter` for full docs\n");
-        printf("  -v, --version    get running version\n");
-        printf("  -m, --message    pass string argument, must be wrapped in quotes\n");
-        printf("\n");
-        printf("\n");
-        break;
-      case 'v':
-        printf("banner-commenter %s\n", PROGRAM_VERSION);
-        break;
-      case 'm':
-        message = optarg;
         printf("here is your message : %s\n", message);
-        break;
-      default:
-        printf("unexpected case\n");
-        return EXIT_FAILURE;
-      
+    case 'h':
+      printf("\n");
+      printf("  -h, --help       help, run `man banner-commenter` for full "
+             "docs\n");
+      printf("  -v, --version    get running version\n");
+      printf("  -m, --message    pass string argument, must be wrapped in "
+             "quotes\n");
+      printf("\n");
+      printf("\n");
+      break;
+
+    case 'v':
+      printf("banner-commenter %s\n", PROGRAM_VERSION);
+      break;
+
+    case 'm':
+      message = optarg;
+      convertUpperCase(message);
+      printf("here is your message : %s\n", message);
+      break;
+
+    default:
+      printf("unexpected case\n");
+      return EXIT_FAILURE;
     }
   }
 
